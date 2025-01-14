f2fa9845f5a548fc53cac7720244e86d2537715d
Marcelo Mendonça <mendonca2709@gmail.com>
2025-01-14 20:05:20 -0300

    chore: log state (#3)

--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,32 @@
 import * as core from '@actions/core';
+import * as fs from 'fs';
 
 export type CacheHitKindState = 'exact' | 'partial' | 'none';
+
+function readStateFile() {
+  const path = process.env['GITHUB_STATE'];
+  if (!path) {
+    core.debug('readStateFile path is empty');
+    return;
+  }
+  core.info(`readFile path ${path}`);
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    core.info(`readFile data: ${data}`);
+  } catch (err) {
+    console.error(err);
+  }
+  [
+    'STATE_path',
+    'STATE_bucket',
+    'STATE_cache-hit-kind',
+    'STATE_skip-upload-on-hit',
+    'STATE_target-file-name',
+    'STATE_root-dir',
+  ].forEach((name) => {
+    core.info(`process.env[${name}]: ${process.env[name] || ''}`);
+  });
+}
 
 export interface State {
   path: string;
@@ -12,7 +38,7 @@ export interface State {
 }
 
 export function saveState(state: State): void {
-  core.debug(`Saving state: ${JSON.stringify(state)}.`);
+  core.info(`[state.ts] Saving state: ${JSON.stringify(state)}.`);
 
   core.saveState('bucket', state.bucket);
   core.saveState('path', state.path);
@@ -20,6 +46,9 @@ export function saveState(state: State): void {
   core.saveState('skip-upload-on-hit', state.skipUploadOnHit);
   core.saveState('target-file-name', state.targetFileName);
   core.saveState('root-dir', state.rootDir);
+
+  core.info(`[state.ts saveState] readStateFile`);
+  readStateFile();
 }
 
 export function getState(): State {
@@ -32,7 +61,8 @@ export function getState(): State {
     rootDir: core.getState('root-dir'),
   };
 
-  core.debug(`Loaded state: ${JSON.stringify(state)}.`);
+  core.info(`[state.ts getState] readStateFile`);
+  readStateFile();
 
   return state;
 }
